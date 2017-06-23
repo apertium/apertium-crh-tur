4a85f80c098d47f9f5c8af9d3dc73bea7c240f22
Francis M. Tyers <ftyers@prompsit.com>
2017-06-23 22:12:27 +0000

    update corpus errors script

---
+++ dev/corpus-errors.sh
@@ -0,0 +1,9 @@
+lang2path=`cat config.log | grep lang2= | sed 's/lang2=/@/g' | cut -f2 -d'@'`
+lang2=`basename $lang2path | sed 's/apertium-//g'`
+pair=`cat config.log | grep ^PACKAGE= | cut -f2 -d"'" | sed 's/apertium-//g'`;
+apertium -d . $pair-postchunk2 2>/dev/null | sed 's/\$/$\n/g' | sort -f | uniq -c | sort -gr > /tmp/$pair.postchunk2
+cat /tmp/$pair.postchunk2 | lt-proc -g $pair.autogen.bin > /tmp/$pair.gen
+paste /tmp/$pair.postchunk2 /tmp/$pair.gen | grep '#' > /tmp/$pair.fail
+cat /tmp/$pair.fail | cut -f2 -d'#' | cut -f1 -d'\' | lt-proc -w $lang2path/$lang2.automorf.bin > /tmp/$pair.$lang2.morf
+paste /tmp/$pair.fail /tmp/$pair.tur.morf 
+paste /tmp/$pair.fail /tmp/$pair.tur.morf | wc -l
