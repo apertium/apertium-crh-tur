906c2d85f27a6e059cc354e28674a14af657b7a6
Francis M. Tyers <ftyers@prompsit.com>
2017-06-27 14:22:26 +0000

    batı

--- dev/corpus-errors.sh
+++ dev/corpus-errors.sh
@@ -1,9 +1,10 @@
 lang2path=`cat config.log | grep lang2= | sed 's/lang2=/@/g' | cut -f2 -d'@'`
 lang2=`basename $lang2path | sed 's/apertium-//g'`
 pair=`cat config.log | grep ^PACKAGE= | cut -f2 -d"'" | sed 's/apertium-//g'`;
-apertium -d . $pair-postchunk2 2>/dev/null | sed 's/\$/$\n/g' | sed 's/^ *//g' | sed 's/ *$//g' | sort -f | uniq -c | sort -gr > /tmp/$pair.postchunk2
-cat /tmp/$pair.postchunk2 | lt-proc -g $pair.autogen.bin > /tmp/$pair.gen
-paste /tmp/$pair.postchunk2 /tmp/$pair.gen | grep '#' > /tmp/$pair.fail
+apertium -d . $pair-postchunk2 2>/dev/null > /tmp/$pair.postchunk2.0
+cat /tmp/$pair.postchunk2.0 | sed 's/\$/$\n/g' | sed 's/^ *//g' | sed 's/ *$//g' | sort -f | uniq -c | sort -gr > /tmp/$pair.postchunk2.1
+cat /tmp/$pair.postchunk2.1 | lt-proc -g $pair.autogen.bin > /tmp/$pair.gen
+paste /tmp/$pair.postchunk2.1 /tmp/$pair.gen | grep '#' > /tmp/$pair.fail
 cat /tmp/$pair.fail | cut -f2 -d'#' | cut -f1 -d'\' | apertium-destxt | lt-proc -w $lang2path/$lang2.automorf.bin | apertium-retxt > /tmp/$pair.$lang2.morf
 paste /tmp/$pair.fail /tmp/$pair.tur.morf 
 paste /tmp/$pair.fail /tmp/$pair.tur.morf | wc -l
