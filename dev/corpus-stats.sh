2f64fd2e4a2910c03beb7ef7bfe7e35866b91a37
Francis M. Tyers <ftyers@prompsit.com>
2017-06-30 09:26:42 +0000

    corpus stats

--- dev/corpus-stats.sh
+++ dev/corpus-stats.sh
@@ -1,7 +1,10 @@
+lang1path=`cat config.log | grep lang1= | sed 's/lang1=/@/g' | sed 's/--with-lang2=/@/g' | cut -f2 -d'@'`
+lang1=`basename $lang1path | sed 's/apertium-//g'`
 lang2path=`cat config.log | grep lang2= | sed 's/lang2=/@/g' | cut -f2 -d'@'`
 lang2=`basename $lang2path | sed 's/apertium-//g'`
 pair=`cat config.log | grep ^PACKAGE= | cut -f2 -d"'" | sed 's/apertium-//g'`;
-apertium -d . $pair-postchunk2 2>/dev/null > /tmp/$pair.postchunk2.0
+
+tee /tmp/$pair.input | apertium -d . $pair-postchunk2 2>/dev/null > /tmp/$pair.postchunk2.0
 cat /tmp/$pair.postchunk2.0 | sed 's/\$/$\n/g' | sed 's/^ *//g' | sed 's/ *$//g' | grep -v '^ *$' > /tmp/$pair.postchunk2.1
 cat /tmp/$pair.postchunk2.1 | lt-proc -g $pair.autogen.bin > /tmp/$pair.gen
 
@@ -9,10 +12,16 @@ total=`cat /tmp/$pair.postchunk2.1 | wc -l`
 generr=`cat /tmp/$pair.gen | grep '#' | wc -l`;
 unkerr=`cat /tmp/$pair.postchunk2.1 | grep '\*' | wc -l`;
 
+cat /tmp/$pair.input | apertium -d $lang1path "$lang1""-morph" | sed 's/\$/$\n/g' | sed 's/^ *//g' | sed 's/ *$//g' | grep -v '^ *$' > /tmp/$pair.morph
 
+untr_total=`cat /tmp/$pair.morph | wc -l`;
+untr_known=`cat /tmp/$pair.morph | grep -v '\*' | wc -l`;
+
+untrimmed=`calc "(($untr_known/$untr_total)*100)" | cut -f2 -d'~' | head -c 5`;
 trimmedclean=`calc "100-((($generr+$unkerr)/$total)*100)" | cut -f2 -d'~' | head -c 5`;
 trimmed=`calc "100-(($unkerr/$total)*100)" | cut -f2 -d'~' | head -c 5`;
 
-echo "$total $unkerr $generr"
-echo "Trimmed: $trimmed %";
-echo "Clean: $trimmedclean %";
+echo "$untr_total $untr_known // $total $unkerr $generr"
+echo -e "Untrimmed:\t$untrimmed %";
+echo -e "Trimmed:\t$trimmed %";
+echo -e "Clean  :\t$trimmedclean %";
