f0eda65ad183f877c72fbb95481276178ca8f6fb
Francis M. Tyers <ftyers@prompsit.com>
2017-07-06 08:45:09 +0000

    transfer hacks for stranded copulas

--- dev/corpus-errors.sh
+++ dev/corpus-errors.sh
@@ -4,7 +4,7 @@ pair=`cat config.log | grep ^PACKAGE= | cut -f2 -d"'" | sed 's/apertium-//g'`;
 apertium -d . $pair-postchunk2 2>/dev/null > /tmp/$pair.postchunk2.0
 cat /tmp/$pair.postchunk2.0 | sed 's/\$/$\n/g' | sed 's/^ *//g' | sed 's/ *$//g' | sort -f | uniq -c | sort -gr > /tmp/$pair.postchunk2.1
 cat /tmp/$pair.postchunk2.1 | lt-proc -g $pair.autogen.bin > /tmp/$pair.gen
-paste /tmp/$pair.postchunk2.1 /tmp/$pair.gen | grep '#' > /tmp/$pair.fail
+paste /tmp/$pair.postchunk2.1 /tmp/$pair.gen | grep -v '#^' | grep '#' > /tmp/$pair.fail
 cat /tmp/$pair.fail | cut -f2 -d'#' | cut -f1 -d'\' | apertium-destxt | lt-proc -w $lang2path/$lang2.automorf.bin | apertium-retxt > /tmp/$pair.$lang2.morf
-paste /tmp/$pair.fail /tmp/$pair.tur.morf 
-paste /tmp/$pair.fail /tmp/$pair.tur.morf | wc -l
+paste /tmp/$pair.fail /tmp/$pair.$lang2.morf 
+paste /tmp/$pair.fail /tmp/$pair.$lang2.morf | wc -l
