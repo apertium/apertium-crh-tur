017d7d4e73d5a85488c7ce69ae1637c5a2fa5247
Francis M. Tyers <ftyers@prompsit.com>
2017-08-21 10:50:46 +0000

    fixes

--- dev/corpus-errors.sh
+++ dev/corpus-errors.sh
@@ -1,7 +1,7 @@
 lang2path=`cat config.log | grep lang2= | sed 's/lang2=/@/g' | cut -f2 -d'@'`
 lang2=`basename $lang2path | sed 's/apertium-//g'`
 pair=`cat config.log | grep ^PACKAGE= | cut -f2 -d"'" | sed 's/apertium-//g'`;
-apertium -d . $pair-postchunk2 2>/dev/null > /tmp/$pair.postchunk2.0
+sed 's/#//g' | apertium -d . $pair-postchunk2 2>/dev/null > /tmp/$pair.postchunk2.0
 cat /tmp/$pair.postchunk2.0 | sed 's/\$/$\n/g' | sed 's/^ *//g' | sed 's/ *$//g' | sort -f | uniq -c | sort -gr > /tmp/$pair.postchunk2.1
 cat /tmp/$pair.postchunk2.1 | lt-proc -g $pair.autogen.bin > /tmp/$pair.gen
 paste /tmp/$pair.postchunk2.1 /tmp/$pair.gen | grep -v '#^' | grep '#' > /tmp/$pair.fail
